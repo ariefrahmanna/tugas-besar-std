75e1dd62428a542120ba7508a0e9355671997a87
Arief Rahman <140642047+ariefrahmanna@users.noreply.github.com>
2023-12-26 15:58:38 +0700

    add menu header functions

--- menu.h
+++ menu.h
@@ -1,6 +1,12 @@
 #ifndef MENU_H_INCLUDED
 #define MENU_H_INCLUDED
 
-
+void menu_organisasi();
+void menu_mahasiswa();
+void menu_prestasi();
+void print_prompt_main_menu();
+void print_prompt_menu_organisasi();
+void print_prompt_menu_mahasiswa();
+void print_prompt_menu_prestasi();
 
 #endif // MENU_H_INCLUDED
