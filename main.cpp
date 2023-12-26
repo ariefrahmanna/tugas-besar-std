0122bce1da222d17119774468a4d9396f5e90978
Arief Rahman <140642047+ariefrahmanna@users.noreply.github.com>
2023-12-26 15:46:44 +0700

    consntruct main menu

--- main.cpp
+++ main.cpp
@@ -1,9 +1,38 @@
 #include "adt.h"
+#include "menu.h"
+#include "utilities.h"
 
 using namespace std;
 
 int main() {
-    cout << "hi hi hi" << endl;
+    list_organisasi L1;
+    list_mahasiswa L2;
+    list_prestasi L3;
+    string input;
+
+    while (true) {
+        clear_screen();
+        // print_prompt();
+        cout << "Pilih [1/2/3/4]: ";
+        cin >> input;
+
+        if (input == "1") {
+            // TODO: organisasi
+        } else if (input == "2") {
+            // TODO: mahasiswa
+        } else if (input == "3") {
+            // TODO: prestasi
+        } else if (input == "4") {
+            break;
+        } else {
+            // TODO
+            // cetak "salah input!"
+            wait_for_enter();
+        }
+    }
+
+    clear_screen();
+    cout << "bye" << endl;
 
     return 0;
 }
