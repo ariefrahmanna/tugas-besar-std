e855a8ab161a1783f4d5f9dac3041d035443ba58
Arief Rahman <140642047+ariefrahmanna@users.noreply.github.com>
2023-12-26 14:56:20 +0700

    add utils functions

---
+++ utilities.cpp
@@ -0,0 +1,19 @@
+#include "utilities.h"
+#include <stdio.h>
+
+void validate_repeat(string &input, string prompt) {
+    input = "";
+
+    while (input != "y" && input != "n" && input != "Y" && input != "N") {
+        cout << prompt;
+        cin >> input;
+    }
+}
+
+void clear_screen() {
+    printf("\033[H\033[2J");
+}
+
+void wait_for_enter() {
+    // TODO
+}
--- utilities.h
+++ utilities.h
@@ -1,6 +1,14 @@
 #ifndef UTILITIES_H_INCLUDED
 #define UTILITIES_H_INCLUDED
 
+#include <iostream>
+using namespace std;
 
+const string WRONG_INPUT_PROMPT = "Pilihan anda salah, silahkan input lagi.";
+const string WAIT_FOR_ENTER_PROMPT = "\nTekan Enter untuk lanjut...";
+
+void validate_repeat(string &input, string prompt);
+void clear_screen();
+void wait_for_enter();
 
 #endif // UTILITIES_H_INCLUDED
